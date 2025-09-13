4ad7692134f8b307216be6a367464cea54e1294b
richards <oliveirafrancarichards@gmail.com>
2025-09-13 19:43:28 -0300

    update somethings

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,3 @@
-const iframeTag = document.querySelector('iframe')
 
 // levar usuário ate sessão sobre mim
 document.querySelectorAll(".btnAboutMe").forEach(e=>{
@@ -18,15 +17,36 @@ document.querySelectorAll('.btnServices').forEach(e=>{
     i.preventDefault()
     
     // rolagem ate sessão "serviços"
-    scrollTo(0,2000)
+    scrollTo(0,2400)
+})})
+document.querySelectorAll('.btnProjects').forEach(e=>{
+    e.addEventListener('click',i=>{
+    // anula reload da tag "a"
+    i.preventDefault()
+    
+    // rolagem ate sessão "serviços"
+    scrollTo(0,1500)
+})})
+document.querySelectorAll('.homeBtn').forEach(e=>{
+    e.addEventListener('click',i=>{
+    // anula reload da tag "a"
+    i.preventDefault()
+    
+    // rolagem ate sessão "serviços"
+    scrollTo(0,0)
 })})
 
+// variaveis para link de direcionamento para whatsapp
 const number = '5527998282603';
 const message = 'Olá gostaria de fazer um orçamento de seu serviço!';
 const url = `https://wa.me/${number}?text=${encodeURIComponent(message)}`;
-document.querySelector('.mainTextInfo button').addEventListener('click',()=>{
-    window.open(url, '_blank')
-})
+
+document.querySelectorAll('.contratarBtn').forEach(e=>{
+    e.addEventListener('click',i=>{
+        // anula reload da tag "a"
+        i.preventDefault()
+        window.open(url, '_blank')
+})})
 
 
 
